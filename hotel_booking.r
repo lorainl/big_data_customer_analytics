9d6dccd77d23046ab1b2f9b7fa1c020944bd40af
lorainl <112580025+lorainl@users.noreply.github.com>
2023-03-02 11:32:02 -0500

    logistic regression using forward and backward variable selection

--- hotel_booking.r
+++ hotel_booking.r
@@ -1,5 +1,6 @@
 #install libraries
 library(ggplot2)
+library(ggpubr)
 library(dplyr)
 library(stargazer)
 library(nnet)
@@ -11,12 +12,19 @@ library(psych)
 library(randomForest)
 library(rpart)
 library(viridis)
+library(readxl)
+library(Hmisc)
+library(mfx)
+library(rcompanion)
+
 #load data
 hotel_bookings <- read.csv("./train.csv")
+hotel_booking<-drop_na(hotel_bookings)
 head(hotel_bookings)
 
 # simple data exploration
 str(hotel_bookings)
+
 # plot booking status by room type
 ggplot(hotel_bookings, aes(x = as.factor(no_of_children),y=booking_status)) +
     stat_summary(fun = "mean", geom = "bar")+ 
@@ -73,6 +81,16 @@ ggplot(hotel_bookings, aes(x = as.factor(no_of_weekend_nights),y=booking_status)
             )
 #does not seem to have significant correlation
 
+#plot booking status by required car parking
+ggplot(hotel_bookings, aes(x = as.factor(required_car_parking_space),y=booking_status)) +
+    stat_summary(fun = "mean", geom = "bar")+ 
+    labs(title = "Percentage of confirmed bookings by required car parking", x = "Required Car Parking", y = "Percentage Confirmed")+
+    theme(plot.title = element_text(hjust = 0.5,size=32),
+            axis.title.x = element_text(size = 24),
+            axis.title.y = element_text(size = 24)
+            )
+#those who required car parking are more likely to cancel, but number is small, we cannot say for sure
+
 #plot average price and booking status
 ggplot(hotel_bookings, aes(x = as.factor(booking_status),y=avg_price_per_room)) +
     geom_boxplot()+ 
@@ -94,4 +112,45 @@ ggplot(hotel_bookings, aes(x = as.factor(booking_status),y=lead_time)) +
 #seems like the lead time is longer for confirmed booking
 cor.test(hotel_bookings$booking_status,hotel_bookings$repeated_guest)
 #correlation is not significant
+cor.test(hotel_bookings$booking_status,hotel_bookings$no_of_previous_cancellations)
+#correlation is not significant
+cor.test(hotel_bookings$booking_status,hotel_bookings$no_of_special_requests)
+#some correlation but weak
+cor.test(hotel_bookings$booking_status,hotel_bookings$avg_price_per_room)
+#some correlation but weak
+cor.test(hotel_bookings$booking_status,hotel_bookings$lead_time)
+#some moderate correlation
+#correlation matrix among all parameters
+rcorr(as.matrix(hotel_bookings[,c("no_of_adults","no_of_children","no_of_weekend_nights","no_of_week_nights",
+                                "type_of_meal_plan","required_car_parking_space","room_type_reserved","lead_time",
+                                "arrival_month","market_segment_type","repeated_guest","no_of_previous_cancellations",
+                                "avg_price_per_room","no_of_special_requests")]))
+#some strong correlation exist between:
+#no_of_children and market_segment_type
+#no_of_weekend_nights and no_of_week_nights
+#weekend_nights and repeated_guest
+#meal_plan and room_type_reserved,number_of_children
+#parking and segment_type,repeated guiests
+#room_type_reserved and no of children and avg price
+#repeated guest and market segment, previous cancellation (but this is expected?)
+#avg price is correlated with number of visitors, room type, repeated guest(neg),special requests
+# will drop average price, market segment, number of children for glm
 
+
+
+
+##===================Logistic Regression===================
+glm_model.all<-glm(booking_status~.-id, data=hotel_bookings, family=binomial)
+stargazer(glm_model.all,type="text",no.space=TRUE)
+glm_model.backward<-step(glm_model.all, direction="backward", trace=0)
+stargazer(glm_model.backward,type="text",no.space=TRUE)
+glm_model.minimal <- glm(booking_status ~ 1, data=hotel_bookings)
+glm_model.forward<- step(glm_model.minimal, scope=list(upper = glm_model.all,lower= glm_model.minimal), direction="forward",trace=0)
+stargazer(glm_model.forward,type="text",no.space=TRUE)
+nagelkerke(glm_model.all)
+nagelkerke(glm_model.backward)#this one gives the highest R^2, but is not great
+nagelkerke(glm_model.forward)
+#calculate mfx
+backward_mfx<-logitmfx(booking_status~. -no_of_previous_bookings_not_canceled, data = hotel_bookings)
+backward_mfx
+#the most important variables are: room type (neg), market segment, number of special requests(neg), number of cancellations
