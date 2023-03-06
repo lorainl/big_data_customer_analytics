7507576d9fc8fea3032054b2867ee679901146d9
lorainl <112580025+lorainl@users.noreply.github.com>
2023-03-06 18:26:59 -0500

    added nnet and tensorflow neural network

--- hotel_booking.r
+++ hotel_booking.r
@@ -19,49 +19,11 @@ library(rcompanion)
 
 #load data
 hotel_bookings <- read.csv("./train.csv")
+test <- read.csv("./test.csv")
 hotel_booking<-drop_na(hotel_bookings)
 head(hotel_bookings)
-
-## ======================== Decision Tree ========================
-#split data into train and test
-set.seed(123)
-train_index <- sample(1:nrow(hotel_bookings), 0.8*nrow(hotel_bookings))
-train <- hotel_bookings[train_index,]
-test <- hotel_bookings[-train_index,]
-#build decision tree
-tree_model_big <- rpart(booking_status ~ ., data = train, method = "class",control = rpart.control(cp = 0))
-tree_model<-tree(booking_status ~ ., data = train, method = "class")
-plotcp(tree_model_big,upper = c("size"))
-#the best tree has ~156 leaves
-tree_156<-prune(tree_model_big,cp=0.00024)
-#tree depth and leaves
-nodes <- as.numeric(rownames(tree_156$frame))
-max(rpart:::tree.depth(nodes))
-sum(tree_156$frme$var == "<leaf>")
-#predicta
-tree_156$y<-test$booking_status
-tree_156$yhat <- predict(tree_156, test, type = "class")
-#accuracy
-mean(tree_156$y == tree_156$yhat)
-#The pruned decision tree has an accuracy of 81.8%
-#test of overfitting
-tree_156$y_train<-predict(tree_156, train, type = "class")
-mean(tree_156$y_train == train$booking_status)
-#this model is not over fitting
-
-## ======================== Random Forest ========================
-#build random forest
-
-rf_model <- randomForest(booking_status ~ ., data = train, importance = TRUE, ntree = 100,maxdepth = 5)
-rf_model.train_predit = predict(rf_model,train)
-rf_model.predict = predict(rf_model,test)
-
-prop.table(table(rf_model.train_predit>0.5,train$booking_status))
-#train accuracy is 98.254%
-prop.table(table(rf_model.predict>0.5,test$booking_status))
-#test accuracy is 82.447%
-#random forest is overfitting no matter the tree depth or number of trees
-#this may not be the best model for this data set
+str(hotel_bookings)
+head(test)
 
 ## ======================== Logistic Regression ========================
 # simple data exploration
@@ -193,3 +155,88 @@ nagelkerke(glm_model.forward)
 backward_mfx<-logitmfx(booking_status~. -no_of_previous_bookings_not_canceled, data = hotel_bookings)
 backward_mfx
 #the most important variables are: room type (neg), market segment, number of special requests(neg), number of cancellations
+
+## ======================== Decision Tree ========================
+#split data into train and test
+set.seed(123)
+train_index <- sample(1:nrow(hotel_bookings), 0.8*nrow(hotel_bookings))
+train <- hotel_bookings[train_index,]
+val <- hotel_bookings[-train_index,]
+#build decision tree
+tree_model_big <- rpart(booking_status ~ ., data = train, method = "class",control = rpart.control(cp = 0))
+tree_model<-tree(booking_status ~ ., data = train, method = "class")
+plotcp(tree_model_big,upper = c("size"))
+#the best tree has ~156 leaves
+tree_156<-prune(tree_model_big,cp=0.00033)
+#tree depth and leaves
+nodes <- as.numeric(rownames(tree_156$frame))
+max(rpart:::tree.depth(nodes))
+sum(tree_156$frame$var == "<leaf>")
+#predict
+tree_156$y<-val$booking_status
+tree_156$yhat <- predict(tree_156, val, type = "class")
+#accuracy
+mean(tree_156$y == tree_156$yhat)
+#The pruned decision tree has an accuracy of 81.8%
+#test of overfitting
+tree_156$y_train<-predict(tree_156, train, type = "class")
+mean(tree_156$y_train == train$booking_status)
+#this model is not over fitting
+#predictions
+tree_156$predict<- predict(tree_156, test, type = "class")
+
+## ======================== Random Forest ========================
+#build random forest
+
+rf_model <- randomForest(booking_status ~ ., data = train, importance = TRUE, ntree = 100,maxdepth = 5)
+rf_model.train_predit = predict(rf_model,train)
+rf_model.predict = predict(rf_model,val)
+
+prop.table(table(rf_model.train_predit>0.5,train$booking_status))
+#train accuracy is 98.254%
+prop.table(table(rf_model.predict>0.5,val$booking_status))
+#test accuracy is 82.447%
+#random forest is overfitting no matter the tree depth or number of trees
+#this may not be the best model for this data set
+
+##===================Neural Network===================
+#set seed
+set.seed(123)
+n <- names(train[,-1])
+f <- as.formula(paste("booking_status ~", paste(n[!n %in% "booking_status"], collapse = " + ")))
+nnet<-neuralnet(f, data=train, hidden = 10)
+nnet.train<-predict(nnet, train)
+nnet.val<-predict(nnet, val)
+prop.table(table(nnet.train>0.5,train$booking_status))
+prop.table(table(nnet.val>0.5,val$booking_status))
+#this is a bad model to predict cancellation, since the calculation time is long for multiple layers, and the accuracy is bad
+
+##===================keras===================
+#install.packages("keras")
+library(tensorflow)
+library(keras)
+data<-train[,-1]
+data<-as.matrix(data)
+labels<-train$booking_status
+data_val<-val[,-1]
+data_val<-as.matrix(data_val)
+val_labels<-val$booking_status
+head(labels)
+model <- keras_model_sequential() %>%
+    layer_dense(units = 5, activation = "relu",input_shape = c(18)) %>%
+    layer_dense(units = 5, activation = "relu") %>%
+    layer_dense(units = 1, activation = "sigmoid")
+model %>% compile(
+  optimizer = 'adam',
+  loss = 'binary_crossentropy',
+  metrics = list('accuracy')
+)
+history<-model %>% fit(
+  data,
+  labels,
+  epochs = 5
+)
+results <- model %>% evaluate(data_val, val_labels)
+plot(history)
+history$metrics
+results
